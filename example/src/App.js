db456ba8a20c7aa8907122e64c06149c5a68d45d
Martin McNeela <martin@edozo.com>
2019-11-19 16:07:49 +0000

    quick updates

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import {Button} from 'test-ui-lib'
+import { Button } from 'test-ui-lib';
 
 /**
  * This should be used as a playground for building components,
  * storybook will be used for displaying components to stakeholders.
  */
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <Button text="I'm a real button!" />
       </div>
-    )
+    );
   }
 }
