b1c45420bfbbed788c2f123fc92f4224d1a7d387
Martin McNeela <martin@edozo.com>
2019-11-25 18:06:19 +0000

    add theme alias colors (#9)

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 
-storiesOf('Button', module).add('Dummy setup', () => <Button text="I am a story" />);
+storiesOf('Fake button', module).add('Fake button', () => (
+  <Button text="I am not a real button, I will need to be replace with the real button" />
+));
