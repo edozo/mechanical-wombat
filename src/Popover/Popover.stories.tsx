503bed43ec91c41a843a6f82e357432578460acc
Zac Robinson <zac@edozo.com>
2019-12-11 13:01:03 +0000

    add onclick to popover

--- src/Popover/Popover.stories.tsx
+++ src/Popover/Popover.stories.tsx
@@ -12,14 +12,67 @@ export default {
   },
 };
 
-export const SingleFormatOption = (): JSX.Element => <Popover>{[{ label: 'PDF' }]}</Popover>;
+export const SingleFormatOption = (): JSX.Element => (
+  <Popover>
+    {[
+      {
+        label: 'PDF',
+        onClick: () => {
+          // do nothing
+        },
+      },
+    ]}
+  </Popover>
+);
 
 export const MultiFormatOption = (): JSX.Element => (
-  <Popover>{[{ label: 'PDF' }, { label: 'JPG' }, { label: 'DOCX' }]}</Popover>
+  <Popover>
+    {[
+      {
+        label: 'PDF',
+        onClick: () => {
+          // do nothing
+        },
+      },
+      {
+        label: 'JPG',
+        onClick: () => {
+          // do nothing
+        },
+      },
+      {
+        label: 'DOCX',
+        onClick: () => {
+          // do nothing
+        },
+      },
+    ]}
+  </Popover>
 );
 
 export const LongListOption = (): JSX.Element => (
-  <Popover>{[{ label: 'THIS IS A LONG LIST OPTION' }, { label: 'JPG' }, { label: 'DOCX' }]}</Popover>
+  <Popover>
+    {[
+      {
+        label: 'THIS IS A LONG LIST OPTION',
+        onClick: () => {
+          // do nothing
+        },
+      },
+      {
+        label: 'JPG with alert onClick',
+        onClick: () => {
+          alert('wakey wakey');
+        },
+      },
+      {
+        label: 'DOCX',
+        onClick: () => {
+          // do nothing
+        },
+      },
+    ]}
+  </Popover>
 );
 
 export const DisabledMultiFormatOption = (): JSX.Element => (
