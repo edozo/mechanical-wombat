f12a66a0fb75e9b81f34d6957256947922091965
Zac Robinson <zacrobinson1992@gmail.com>
2019-12-17 17:26:07 +0000

    Feature/handle popover click (#30)
    * Rename popover conponent
    
    * Add click handler component
    
    * Update stories

--- src/Popover/Popover.tsx
+++ src/Popover/Popover.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
-import { StyledPopover, StyleProps } from './Popover.styles';
+import { useOnClickOutside } from '../hooks';
 
-export interface Props extends StyleProps {
+const { useRef } = React;
+
+export interface Props {
+  isOpen: boolean;
+  setIsOpen: (param: boolean) => void;
   children: React.ReactNode;
 }
 
-export const Popover = (props: Props): JSX.Element => <StyledPopover {...props}>{props.children}</StyledPopover>;
+export const Popover = (props: Props): JSX.Element => {
+  const { isOpen, setIsOpen, children } = props;
+  const node = useRef(null);
+  useOnClickOutside(node, () => setIsOpen(false));
+
+  return <div ref={node}>{isOpen && children}</div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ export { GlobalStyle } from './globalStyle';
 export { Example } from './Example';
 export { Button } from './Button';
 export { TableDownloadButton } from './TableDownloadButton';
+export { PopoverContent } from './PopoverContent';
 export { Popover } from './Popover';
 
 export { default } from './ThemeProvider';
