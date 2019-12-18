5c2fe23405d40665ca1c04712cc3d114c72f4a16
Zac Robinson <zacrobinson1992@gmail.com>
2019-12-18 12:24:36 +0000

    Export forgotten components (#32)
    * Export forgotten components
    
    * export component

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ export { Button } from './Button';
 export { TableDownloadButton } from './TableDownloadButton';
 export { PopoverContent } from './PopoverContent';
 export { Popover } from './Popover';
+export { List, ListItem } from './List';
 
 export { default } from './ThemeProvider';
