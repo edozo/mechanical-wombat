8ef950c7b8fea06dc0a9043e4b681899d9ed55c6
martinmcneela <mcneela86@gmail.com>
2020-03-30 15:14:14 +0100

    updating to beeter example app and rename it

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -5,6 +5,7 @@ import { Badge } from 'test-ui-lib';
  * This should be used as a playground for building components,
  * storybook will be used for displaying components to stakeholders.
  */
+
 export const App = () => {
   return (
     <div style={{ width: '100vw', height: '100vh', background: '#ccc'}}>
