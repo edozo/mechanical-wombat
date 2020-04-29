5f856008c9a8c9bd8b1da05e0fc45b969a47a45c
martinmcneela <mcneela86@gmail.com>
2020-04-29 10:22:16 +0100

    Adding box sizing to stop padding breaking the layout

--- src/globalStyle.storybook.ts
+++ src/globalStyle.storybook.ts
@@ -4,7 +4,8 @@ import { defaultTheme } from './defaultTheme';
 const StorybookGlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700'); /* update to match the design system */
   * {
-    font-family: ${defaultTheme.font.family.main}
+    font-family: ${defaultTheme.font.family.main};
+    box-sizing: border-box;
   }
 `;
 
