267b9967b4902f142e2f3b59db90715caf9b28e0
martinmcneela <mcneela86@gmail.com>
2020-05-19 19:38:19 +0100

    Update switch and add small size

--- src/Switch/SwitchWrapper.jsx
+++ src/Switch/SwitchWrapper.jsx
@@ -2,14 +2,14 @@
 import React, { useState } from 'react';
 import { Switch } from './Switch';
 
-export const SwitchWrapper = ({ defaultToggleValue, small, disabled, id, onText, offText }) => {
+export const SwitchWrapper = ({ defaultToggleValue, size, disabled, id, onText, offText }) => {
   const [isChecked, setIsChecked] = useState(defaultToggleValue || false);
   return (
     <Switch
       currentValue={isChecked}
       onChange={() => setIsChecked(!isChecked)}
       id={id}
-      small={small}
+      size={size}
       disabled={disabled}
       onText={onText}
       offText={offText}
