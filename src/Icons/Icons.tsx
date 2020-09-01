8a7a6d74d442415832a3c7df1e8adaed23b0baa7
Zac Robinson <zac@edozo.com>
2020-09-01 16:17:19 +0100

    fix PR comments

--- src/Icons/Icons.tsx
+++ src/Icons/Icons.tsx
@@ -49,8 +49,8 @@ import hatchLUUrl, { ReactComponent as HatchLU } from './icons/hatch-lu.svg';
 import searchUrl, { ReactComponent as Search } from './icons/search.svg';
 import stackUrl, { ReactComponent as Stack } from './icons/stack.svg';
 import centerMapUrl, { ReactComponent as CenterMap } from './icons/center-map.svg';
-import HMarkUrl, { ReactComponent as HMark } from './icons/H-mark.svg';
-import TMarkUrl, { ReactComponent as TMark } from './icons/T-mark.svg';
+import hMarkUrl, { ReactComponent as HMark } from './icons/H-mark.svg';
+import tMarkUrl, { ReactComponent as TMark } from './icons/T-mark.svg';
 
 const DEFAULT_ICON_COLOR = '#4c4c4c';
 
@@ -419,11 +419,11 @@ const HMarkIcon: React.FC<Props> = ({ size = 'large', fill = DEFAULT_ICON_COLOR,
     <HMark fill={fill} {...props} />
   </IconWrapper>
 );
-export { HMarkIcon, HMarkUrl };
+export { HMarkIcon, hMarkUrl };
 
 const TMarkIcon: React.FC<Props> = ({ size = 'large', fill = DEFAULT_ICON_COLOR, ...props }) => (
   <IconWrapper size={size}>
     <TMark fill={fill} {...props} />
   </IconWrapper>
 );
-export { TMarkIcon, TMarkUrl };
+export { TMarkIcon, tMarkUrl };
