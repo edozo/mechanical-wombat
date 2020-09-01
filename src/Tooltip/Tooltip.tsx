eb8a38c9c86346d7d898537122e03df3c6e87155
Martin McNeela <martin@edozo.com>
2020-09-01 13:41:08 +0100

    set tooltip default options (#87)

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import Tippy from '@tippyjs/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'tippy.js/dist/tippy.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'tippy.js/animations/shift-away.css';
 
 type TooltipProps = {
   children: React.ReactElement;
   content: React.ReactElement;
 };
+
+Tippy.defaultProps = {
+  animation: 'shift-away',
+  delay: [100, 0],
+  inertia: true,
+  placement: 'bottom',
+};
+
 export const Tooltip: React.FC<TooltipProps> = ({ content, children, ...rest }) => (
-  <Tippy {...rest} content={content}>
+  <Tippy content={content} {...rest}>
     {children}
   </Tippy>
 );
