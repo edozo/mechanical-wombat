85b1397bc54e041323ca7360ff09367bcc98860e
Martin McNeela <martin@edozo.com>
2020-10-14 14:54:19 +0100

    tech/move some global styles out of scss (#100)
    * Move some SCSS global styles into global styles
    
    * bump version

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -4,17 +4,32 @@ import { defaultTheme } from './defaultTheme';
 
 const rebootOptions = {
   fontFamilyBase: defaultTheme.font.family.main,
-  // TODO: Replace these with correct values when in theme
-  // headingsMarginBottom: space[3],
-  // paragraphMarginBottom: space[4],
-  // labelMarginBottom: space[3],
-  // lineHeightBase: font.lineHeights.display,
 };
 
 const GlobalStyle = createGlobalStyle`
   ${reboot(rebootOptions)}
-  * {
+  *, *:before, *:after {
     box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: ${defaultTheme.font.family.main};
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${defaultTheme.font.family.secondary};
+    font-weight: 600;
+  }
+
+  /* TODO: Remove once navbar, navbar items, links and buttons are included as components */
+  p, a {
+    color: ${defaultTheme.colors.grayDarker};
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
 
