66d39bbbeb23e9f79214aabf119c9b5ebf437e86
Martin <mcneela86@gmail.com>
2020-10-14 11:52:32 +0100

    remove comments

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -4,11 +4,6 @@ import { defaultTheme } from './defaultTheme';
 
 const rebootOptions = {
   fontFamilyBase: defaultTheme.font.family.main,
-  // TODO: Replace these with correct values when in theme
-  // headingsMarginBottom: space[3],
-  // paragraphMarginBottom: space[4],
-  // labelMarginBottom: space[3],
-  // lineHeightBase: font.lineHeights.display,
 };
 
 const GlobalStyle = createGlobalStyle`
