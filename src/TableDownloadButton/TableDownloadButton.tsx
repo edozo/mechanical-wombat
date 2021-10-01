d07cb010fb39714089dc54aaf811ee741afd80ab
Zac Robinson <zac@edozo.com>
2021-10-01 13:55:39 +0100

    use some html props (#155)

--- src/TableDownloadButton/TableDownloadButton.tsx
+++ src/TableDownloadButton/TableDownloadButton.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { TableActionButton } from './TableDownloadButton.styles';
 
-export interface TableDownloadButtonProps {
+export interface TableDownloadButtonProps extends React.HTMLProps<HTMLButtonElement> {
   variant: string;
   disabled?: boolean;
 }
+
 export const TableDownloadButton: React.FC<TableDownloadButtonProps> = props => {
   const { children, ...rest } = props;
   return <TableActionButton {...rest}>{children}</TableActionButton>;
