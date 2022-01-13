5ffcd489a41a591e8042c454784df78d167eeb44
Martin McNeela <martin@edozo.com>
2022-01-13 13:54:27 +0000

    change input component (#173)
    * change input component
    
    * speeling
    
    * remove readonly
    
    * update style
    
    * bump

---
+++ src/InputGroup/Item/Item.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/macro';
+
+export interface ItemProps {
+  backgroundColor?: string;
+}
+
+export const StyledItem = styled.div<ItemProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  background-color: ${p => p.backgroundColor};
+  border-top: ${p => `1px solid ${p.theme.colors.grayDark}`};
+  border-bottom: ${p => `1px solid ${p.theme.colors.grayDark}`};
+  svg {
+    margin: 0 8px;
+    width: ${p => p.theme.icons.small};
+    height: ${p => p.theme.icons.small};
+  }
+`;
