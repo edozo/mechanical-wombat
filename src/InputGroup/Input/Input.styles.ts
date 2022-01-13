b3e5efc4fc6a57bb9867b81635f724a88387a80d
Martin <mcneela86@gmail.com>
2022-01-13 13:59:16 +0000

    remove macro styled-components

--- src/InputGroup/Input/Input.styles.ts
+++ src/InputGroup/Input/Input.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 
 export const StyledInput = styled.input`
   flex: 1;
--- src/InputGroup/InputGroup.styles.ts
+++ src/InputGroup/InputGroup.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components/macro';
+import styled, { css } from 'styled-components';
 import { Context } from './InputGroup';
 
 export const StyledInputGroup = styled.div<Context>`
