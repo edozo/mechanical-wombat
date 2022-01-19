18e7fb19247f90837e9405076dffc57768b11364
Izabela Pikula <izapik@Izabelas-MacBook-Pro.local>
2022-01-19 16:20:45 +0000

    Styled platform switch

--- src/Header/Header.styles.ts
+++ src/Header/Header.styles.ts
@@ -20,8 +20,36 @@ export const StyledHeader = styled.div`
 
 export const StyledButtonTitle = styled.div`
   font-family: ${p => p.theme.font.family.main};
-  font-size: ${p => p.theme.spacing.small};
+  font-size: ${p => p.theme.font.size.text.base};
+  line-height: ${p => p.theme.font.lineHeight.text.base};
   font-weight: ${p => p.theme.font.weight.bold};
-  line-height: ${p => p.theme.spacing.base};
   color: #060048;
+  display: flex;
+  margin-left: ${p => p.theme.spacing.small};
+`;
+
+export const StyledText = styled.span`
+  font-family: ${p => p.theme.font.family.main};
+`;
+
+export const ItemWrapper = styled.span`
+  display: grid;
+  grid-template-columns: 220px auto;
+  align-items: center;
+  font-family: ${p => p.theme.font.family.main};
+  font-size: ${p => p.theme.font.size.text.small};
+  font-weight: ${p => p.theme.font.weight.regular};
+`;
+
+export const ListItemWrapper = styled.div`
+  border-bottom: 1px solid ${p => p.theme.colors.gray};
+`;
+
+export const SwitchAppWrapper = styled.span`
+  display: grid;
+  grid-template-columns: 10px auto;
+  svg {
+    margin-top: 4px;
+    margin-left: -6px;
+  }
 `;
