b778f5fc8040dde3276030eee2bf488087a012c8
Martin <mcneela86@gmail.com>
2025-02-07 15:50:22 +0000

    fix failing tests in app

--- src/TableDownloadButton/TableDownloadButton.tsx
+++ src/TableDownloadButton/TableDownloadButton.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import { ButtonHTMLAttributes } from 'react';
 import { TableActionButton } from './TableDownloadButton.styles';
 
-export interface TableDownloadButtonProps extends React.HTMLProps<HTMLButtonElement> {
+export interface TableDownloadButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant: 'primary' | 'secondary';
   disabled?: boolean;
   background?: string;
+  children: React.ReactNode;
 }
 
-export const TableDownloadButton: React.FC<TableDownloadButtonProps> = ({ children, variant, background }) => {
+export const TableDownloadButton: React.FC<TableDownloadButtonProps> = props => {
+  const { children, ...rest } = props;
   return (
-    <TableActionButton variant={variant} background={background}>
+    <TableActionButton type="button" {...rest}>
       {children}
     </TableActionButton>
   );
