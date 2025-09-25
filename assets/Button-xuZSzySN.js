d9aba4eb621d86001c66f9bb01502e44e17f43ed
Mike Ray <mikeray1990@gmail.com>
2025-09-25 14:53:42 +0100

    Updates

---
+++ assets/Button-xuZSzySN.js
@@ -0,0 +1,53 @@
+import{l as r,d as o,r as a,j as t}from"./iframe-BGMA8d48.js";const l=o.button`
+  font-family: ${e=>e.theme.font.family.main};
+  border: 2px solid ${e=>e.theme.colors.aliases[e.color||"primary"]};
+  background-color: ${e=>e.theme.colors.aliases[e.color||"primary"]};
+  border-radius: ${e=>e.theme.borderRadius[e.borderRadius||"standard"]};
+  color: ${e=>e.theme.colors.white};
+  padding: ${e=>e.theme.spacing.xsmall} ${e=>e.theme.spacing.base}; /* Get from theme */
+  font-size: 1.2em; /* Get from theme */
+  transition: all 0.2s; /* Get from theme */
+  &:hover {
+    background-color: ${e=>e.theme.colors.aliases[e.color?`${e.color}Light`:"primaryLight"]};
+    border-color: ${e=>e.theme.colors.aliases[e.color?`${e.color}Light`:"primaryLight"]};
+  }
+  &:focus {
+    background-color: ${e=>e.theme.colors.aliases[e.color&&`${e.color}Light`||"primaryLight"]};
+    border-color: ${e=>e.theme.colors.aliases[e.color&&`${e.color}Dark`||"primaryDark"]};
+    outline: none;
+  }
+  &:active {
+    background-color: ${e=>e.theme.colors.aliases[e.color&&`${e.color}Dark`||"primaryDark"]};
+    border-color: ${e=>e.theme.colors.aliases[e.color&&`${e.color}Dark`||"primaryDark"]};
+    outline: none;
+  }
+
+  ${e=>e.variant==="outline"&&r`
+      background-color: transparent;
+      color: ${e.theme.colors.aliases[e.color||"primary"]};
+      &:hover,
+      &:focus,
+      &:active {
+        color: ${e.theme.colors.white};
+      }
+    `};
+
+  ${e=>e.variant==="ghost"&&r`
+      background-color: transparent;
+      border: 2px solid transparent;
+      color: ${e.theme.colors.aliases[e.color||"primary"]};
+      &:hover,
+      &:focus,
+      &:active {
+        color: ${e.theme.colors.white};
+      }
+    `};
+
+  &:disabled,
+  &[disabled] {
+    background-color: ${e=>e.theme.colors.gray};
+    border-color: ${e=>e.theme.colors.gray};
+    color: ${e=>e.theme.colors.grayLighter};
+  }
+`,s=e=>(a.useEffect(()=>{},[]),t.jsx(l,{...e,children:e.children}));s.__docgenInfo={description:`@deprecated This Button component is deprecated and will be removed in the next major version.
+Please use the new ButtonV2 component instead.`,methods:[],displayName:"Button",props:{color:{required:!1,tsType:{name:"union",raw:"'primary' | 'secondary'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"}]},description:""},variant:{required:!1,tsType:{name:"union",raw:"'standard' | 'outline' | 'ghost'",elements:[{name:"literal",value:"'standard'"},{name:"literal",value:"'outline'"},{name:"literal",value:"'ghost'"}]},description:""},borderRadius:{required:!1,tsType:{name:"union",raw:"'small' | 'standard' | 'large'",elements:[{name:"literal",value:"'small'"},{name:"literal",value:"'standard'"},{name:"literal",value:"'large'"}]},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};export{s as B};
---
+++ assets/PopoverContent.stories-N8EFI3rT.js
@@ -0,0 +1,18 @@
+import{d as g,j as t}from"./iframe-BGMA8d48.js";import{L as s}from"./List-DdVqvO8j.js";import"./preload-helper-C1FmrZbK.js";const x=g.div`
+  box-shadow: ${e=>e.theme.boxShadow.standard};
+  background-color: ${e=>e.theme.colors.white};
+  position: relative;
+  display: inline-block;
+  border-radius: ${e=>e.theme.borderRadius[e.radius||"standard"]};
+
+  &:after {
+    content: '';
+    border-left: 8px solid transparent;
+    border-right: 8px solid transparent;
+    position: absolute;
+    right: 13px;
+    border-bottom: 8px solid ${e=>e.theme.colors.white};
+    top: -8px;
+    bottom: auto;
+  }
+`,a=e=>{const{children:u,...h}=e;return t.jsx(x,{...h,children:u})};a.__docgenInfo={description:"",methods:[],displayName:"PopoverContent",props:{radius:{required:!1,tsType:{name:"union",raw:"'small' | 'standard' | 'large'",elements:[{name:"literal",value:"'small'"},{name:"literal",value:"'standard'"},{name:"literal",value:"'large'"}]},description:""},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const y={title:"Components/PopoverContent",argTypes:{onChange:{action:"changed"}},component:a},m=e=>t.jsx(a,{...e}),r=m.bind({});r.args={radius:"small",children:t.jsx("div",{style:{padding:"8px"},children:"This is my popoverContent"})};const o=m.bind({});o.args={children:t.jsxs(s,{children:[t.jsx(s.Item,{children:"Non-clicky item"}),t.jsx(s.Item,{disabled:!0,children:"Disabled item"})]})};var n,i,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"args => <PopoverContent {...args} />",...(d=(i=r.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var l,p,c;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:"args => <PopoverContent {...args} />",...(c=(p=o.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};const f=["Simple","ListContent"];export{o as ListContent,r as Simple,f as __namedExportsOrder,y as default};
