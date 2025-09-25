3eb7e82029e3f76438b546ca4706dde5e658a352
CircleCI <ci@circleci.com>
2025-09-25 14:14:03 +0000

    Updates

---
+++ assets/Space.stories-DI0zrKAq.js
@@ -0,0 +1,6 @@
+import{j as s}from"./iframe-CAf_PyMu.js";import{D as o,a as l}from"./DisplayBox.styles-DXKn6ios.js";import{D as x,a as m}from"./DisplayBox-Bs0HCOxp.js";import"./preload-helper-C1FmrZbK.js";const y={title:"Theme/Space"},n=[{size:"4px",name:"xxsmall"},{size:"8px",name:"xsmall"},{size:"16px",name:"small"},{size:"24px",name:"base"},{size:"32px",name:"large"},{size:"48px",name:"xlarge"},{size:"64px",name:"xxlarge"}],e=()=>s.jsx(x,{children:n.map(a=>s.jsxs(m,{size:a.size,borderRadius:"xsmall",children:[s.jsx(o,{children:a.name}),s.jsx(l,{children:a.size})]}))});e.__docgenInfo={description:"",methods:[],displayName:"Space"};var i,p,r;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`() => <DisplayBoxWrapper>
+    {spaces.map(space => <DisplayBox size={space.size} borderRadius="xsmall">
+        <DisplayBoxSubTitle>{space.name}</DisplayBoxSubTitle>
+        <DisplayBoxTitle>{space.size}</DisplayBoxTitle>
+      </DisplayBox>)}
+  </DisplayBoxWrapper>`,...(r=(p=e.parameters)==null?void 0:p.docs)==null?void 0:r.source}}};const z=["Space"];export{e as Space,z as __namedExportsOrder,y as default};
--- assets/Tooltip-PInhHCrj.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./iframe-C_eeFh7-.js";import{i as o}from"./shift-away-Il-T_-gI.js";o.defaultProps={animation:"shift-away",delay:[100,0],inertia:!0,placement:"bottom"};const s=({content:t,children:i,...a})=>e.jsx(o,{content:t,...a,children:i});s.__docgenInfo={description:"",methods:[],displayName:"Tooltip"};export{s as T};
