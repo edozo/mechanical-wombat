ede17a67a22176f744fe486c8524660d70e71f36
Mike Ray <mikeray1990@gmail.com>
2025-09-25 14:58:25 +0100

    Updates

---
+++ assets/Tooltip.stories-B0on7TBH.js
@@ -0,0 +1,14 @@
+import{j as t}from"./iframe-C_eeFh7-.js";import{T as n}from"./Tooltip-PInhHCrj.js";import"./preload-helper-C1FmrZbK.js";import"./shift-away-Il-T_-gI.js";import"./index-DSnMejWY.js";const v={title:"Components/Tooltip",component:n},e=()=>t.jsx(n,{content:t.jsx("div",{children:t.jsx("span",{children:"Tooltip"})}),children:t.jsx("button",{type:"button",children:"the child"})}),o=()=>t.jsx("div",{style:{minHeight:"50vh"},children:t.jsx(n,{interactive:!0,interactiveBorder:10,content:t.jsxs("div",{children:[t.jsx("span",{children:"Tooltip"}),t.jsx("button",{type:"button",children:"content"})]}),children:t.jsx("button",{type:"button",children:"the child"})})});e.__docgenInfo={description:"",methods:[],displayName:"Basic"};o.__docgenInfo={description:"",methods:[],displayName:"Interactive"};var i,r,s;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`() => <Tooltip content={<div>
+        <span>Tooltip</span>
+      </div>}>
+    <button type="button">the child</button>
+  </Tooltip>`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};var c,p,a;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`() => <div style={{
+  minHeight: '50vh'
+}}>
+    <Tooltip interactive interactiveBorder={10} content={<div>
+          <span>Tooltip</span>
+          <button type="button">content</button>
+        </div>}>
+      <button type="button">the child</button>
+    </Tooltip>
+  </div>`,...(a=(p=o.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};const b=["Basic","Interactive"];export{e as Basic,o as Interactive,b as __namedExportsOrder,v as default};
