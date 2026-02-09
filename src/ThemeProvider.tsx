cf301915b42d14d01fe31a20e5525898203c0160
M <m@Ms-MacBook-Air.local>
2026-02-09 16:13:41 +0000

    Remove theme override

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,22 +1,12 @@
-import { ThemeProvider, DefaultTheme } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './defaultTheme';
 
 const ComponentLibThemeProvider = ({
-  theme,
   children,
 }: {
-  theme: DefaultTheme;
   children: React.ReactNode;
 }): JSX.Element => {
-  const mergedTheme: DefaultTheme = {
-    ...defaultTheme,
-    ...theme,
-    colors: {
-      ...defaultTheme.colors,
-      ...theme.colors,
-    },
-  };
-  return <ThemeProvider theme={mergedTheme}>{children}</ThemeProvider>;
+  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
 };
 
 export { ComponentLibThemeProvider as ThemeProvider };
